c7c672abdbb6ec114148a7927ac3a2f0e2c9bde4
ranoooooooooooo <rubbed1it9with6the9bow@gmail.com>
2024-01-30 16:40:00 +0900

    Itemモデル・Itemsテーブル作成

---
+++ app/models/item.rb
@@ -0,0 +1,8 @@
+class Item < ApplicationRecord
+  validates :item_name, :item_description, :item_category_id, :item_status_id,
+            :delivery_fee_bearer_id, :prefecture_id, :days_to_ship_id, :price,
+            presence: true
+  belongs_to :user
+  # has_one :order
+  # has_one_attached :image
+end
---
+++ db/migrate/20240130071543_create_items.rb
@@ -0,0 +1,17 @@
+class CreateItems < ActiveRecord::Migration[7.0]
+  def change
+    create_table :items do |t|
+
+      t.string     :item_name,              null: false
+      t.text       :item_description,       null: false
+      t.integer    :item_category_id,       null: false
+      t.integer    :item_status_id,         null: false
+      t.integer    :delivery_fee_bearer_id, null: false
+      t.integer    :prefecture_id,          null: false
+      t.integer    :days_to_ship_id,        null: false
+      t.integer    :price,                  null: false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
