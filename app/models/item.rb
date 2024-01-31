db425157e289ad0b1ce40a613d1f75a0bf34009c
ranoooooooooooo <rubbed1it9with6the9bow@gmail.com>
2024-01-31 12:14:34 +0900

    Itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,12 @@ class Item < ApplicationRecord
   # has_one :order
   has_one_attached :image
 
+  validates :price,
+            numericality:{ only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999,
+            message: "価格は¥300以上、¥9,999,999以下で設定してください"  }
+  validates :price,
+            format: { with: /\A[0-9]+\z/, message: "価格は半角数字で設定してください" }
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   has_one :item_category_id, :item_status_id,
           :delivery_fee_bearer_id, :prefecture_id, :days_to_ship_id
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,8 @@ class User < ApplicationRecord
 
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, presence: true
 
-  with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "パスワードは半角英数字のみ、半角英数字混合で設定してください" } do
-    validates :password
-  end
+  validates :password,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "パスワードは半角英数字のみ、半角英数字混合で設定してください" }
 
   with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "お名前は全角文字で入力してください" } do
     validates :last_name
