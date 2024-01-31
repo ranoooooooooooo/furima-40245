122a7d283903b194380f46cdd9e530df5c9058a9
ranoooooooooooo <rubbed1it9with6the9bow@gmail.com>
2024-01-31 12:20:51 +0900

    ルーティング、Itemsコントローラー記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,23 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+    @items = Item.includes(:user)
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
   private
 
   def item_params
