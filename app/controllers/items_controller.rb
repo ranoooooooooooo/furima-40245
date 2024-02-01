441dc20c4e0a50fe656267208aa00506bb422d93
ranoooooooooooo <rubbed1it9with6the9bow@gmail.com>
2024-02-01 15:55:18 +0900

    ルーティング、コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -19,15 +20,33 @@ class ItemsController < ApplicationController
   end
   
   def show
-    @item = Item.find(params[:id])
   end
 
+  def edit
+    unless current_user.id == @item.user_id
+      redirect_to user_session_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
   private
 
   def item_params
     params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_status_id,
     :delivery_fee_bearer_id, :prefecture_id, :days_to_ship_id, :price, :image)
     .merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
 end
