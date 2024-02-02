410931f36b703b42aa9b722f77a6dbfc24344bde
ranoooooooooooo <rubbed1it9with6the9bow@gmail.com>
2024-02-02 17:57:10 +0900

    購入に付随する各機能 実装（ログイン状況による条件分岐等々）

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,16 +1,22 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
 
   def index
-    @order_form = OrderAddressForm.new
+    if current_user.id != @item.user_id && !@item.order.present?
+      @order_form = OrderAddressForm.new
+    else
+      redirect_to root_path
+    end
   end
 
   def create
     #@order_form = OrderAddressForm.new(item_params)
+    @order_form = OrderAddressForm.new(order_params)
 
-    if @order_form.present?
+    if @order_form.valid?
       @order_form.save
-      redirect_to root_path, notice: 'Order was successfully created.'
+      redirect_to root_path
     else
       render :index, status: :unprocessable_entity
     end
@@ -18,11 +24,16 @@ class OrdersController < ApplicationController
 
   private
 
-  # def item_params
-    # params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_status_id,
-    # :delivery_fee_bearer_id, :prefecture_id, :days_to_ship_id, :price, :image)
-    # .merge(user_id: current_user.id)
-  # end
+  #def item_params
+    #params.require(:item).permit(:item_name, :item_description, :item_category_id, :item_status_id,
+    #:delivery_fee_bearer_id, :prefecture_id, :days_to_ship_id, :price, :image)
+    #.merge(user_id: current_user.id)
+  #end
+
+  def order_params
+    params.require(:order_address_form).permit(:postcode, :prefecture_id, :city, :street, :building, :phone_number)
+    .merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 
   def set_item
     @item = Item.find(params[:item_id])
--- app/models/order_address_form.rb
+++ app/models/order_address_form.rb
@@ -1,12 +1,18 @@
 class OrderAddressForm
   include ActiveModel::Model
-  attr_accessor :user_id,:item_id,:postcode,:prefecture_id,:city,
-                :street,:building,:phone_number,:order_id
+  attr_accessor :user_id, :item_id, :postcode, :prefecture_id,
+                :city, :street, :building, :phone_number, :order_id
 
-  validates :user_id,:item_id,:postcode,:prefecture_id,:city,
-            :street,:building,:phone_number,:order_id,
-            presence: true
-
+  with_options presence: true do
+    validates :postcode, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "半角数字でハイフンを含めて入力してください"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "都道府県を選択してください" }
+    validates :city
+    validates :street
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: '半角数字で10桁以上11桁以内で入力してください'}
+    validates :user_id
+    validates :item_id
+  end
+  
   def save
     if valid?
       persist!
